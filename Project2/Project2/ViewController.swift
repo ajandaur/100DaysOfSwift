2a571953f1f1557e2c2cf90069ba354b70ef3ed5
ajandaur <anmol.jandaur@gmail.com>
2022-01-29 15:19:34 -0800

    Started Project 2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Anmol  Jandaur on 1/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
