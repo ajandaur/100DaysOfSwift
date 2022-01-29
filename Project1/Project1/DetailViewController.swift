9db1587231e9024197b42cec08a837b48e54bf0d
ajandaur <anmol.jandaur@gmail.com>
2022-01-28 22:32:44 -0800

    Project 1 Challenges

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Anmol  Jandaur on 5/16/20.
+//  Copyright © 2020 Anmol  Jandaur. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    // CHALLENGE: Rather than show image names in the detail title bar, show “Picture X of Y”, where Y is the total number of images and X is the selected picture’s position in the array.
+    
+    // create properties for selected picture position and total picture count
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        // if for some reason selectedImage is nil (which it should never be, in theory) then the imageView.image line will never be executed. If it has a value, it will be placed into imageToLoad, then passed to UIImage and loaded
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# 100DaysOfSwift
+
+This is my repo full of projects from the course, 100 Days Of Swift Challenge by Paul Hudson.
+
+## Status
+
+Type               | Number  | Completion
+:---               |  :---:  |   :---:
+Projects           |  1 / 30 | 3.3%
+Milestone Projects |  0 / 11 | 0%
+Final Exam         |  0 / 1  | 0%
+
+## Demos (COMING SOON)
+
